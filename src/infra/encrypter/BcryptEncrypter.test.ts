20c49679809e81115dc2645518b5d655b0c3a64c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-06-27 21:39:57 -0300

    test: add 'should return a hash on success'

--- src/infra/encrypter/BcryptEncrypter.test.ts
+++ src/infra/encrypter/BcryptEncrypter.test.ts
@@ -2,6 +2,12 @@ import bcrypt from 'bcrypt'
 
 import { BcryptEncrypter } from 'src/infra/encrypter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hashed_value'))
+  }
+}))
+
 const salt = 12
 const makeSut = () => new BcryptEncrypter(salt)
 
@@ -14,5 +20,13 @@ describe('BcryptEncrypter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const sut = makeSut()
+
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hashed_value')
   })
 })
