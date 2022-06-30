e9d03b0b3361e37eb256120b1ce294f04613f7cc
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-06-29 22:51:05 -0300

    chore: add base dev docker-compose config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/pols_for_devs_server',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/polls_for_devs_server_mongo',
   port: process.env.PORT || 3333
 }
