1d0883ae4086e2dfd4e6ee9214b905a0a8301e92
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-10-09 01:38:28 -0300

    feat: add LogErrorRepository as dep to ControllerWithLogDecorator composition

--- src/main/factories/signUp.ts
+++ src/main/factories/signUp.ts
@@ -1,6 +1,6 @@
 import { DbAddAccountUseCase } from 'src/data'
 
-import { BcryptEncrypter, MongoDbAccountRepository } from 'src/infra'
+import { BcryptEncrypter, MongoDbAccountRepository, MongoDbLogErrorRepository } from 'src/infra'
 
 import { ControllerWithLogDecorator } from 'src/main/decorators'
 
@@ -11,11 +11,12 @@ import { RegexEmailValidator } from 'src/validation'
 export const makeSignUpController = (): Controller => {
   const encrypter = new BcryptEncrypter()
   const addAccountRepository = new MongoDbAccountRepository()
+  const logErrorRepository = new MongoDbLogErrorRepository()
 
   const emailValidator = new RegexEmailValidator()
   const addAccountUseCase = new DbAddAccountUseCase(encrypter, addAccountRepository)
 
   const signUpController = new SignUpController(emailValidator, addAccountUseCase)
 
-  return new ControllerWithLogDecorator(signUpController)
+  return new ControllerWithLogDecorator(signUpController, logErrorRepository)
 }
