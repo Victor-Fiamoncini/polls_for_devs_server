e078edf69e42f9f5861956b7a3b946dfb92db35b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-12-01 22:44:17 -0300

    feat: add base ValidatorComposite

---
+++ src/validation/validators/ValidatorComposite.ts
@@ -0,0 +1,13 @@
+import { Validator } from 'src/validation'
+
+export class ValidatorComposite implements Validator {
+  constructor (private readonly validators: Validator[]) {}
+
+  validate (input: any): Error {
+    for (const validator of this.validators) {
+      const error = validator.validate(input)
+
+      if (error) return error
+    }
+  }
+}
