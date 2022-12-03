8bf8e1e9cf726a1ab5bd256c9fb38bd7b98fdedd
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-12-02 23:42:08 -0300

    test: add base CompareFieldsValidator tests

---
+++ src/validation/validators/CompareFieldsValidator.test.ts
@@ -0,0 +1,23 @@
+import { InvalidParamError } from 'src/presentation'
+
+import { CompareFieldsValidator } from 'src/validation'
+
+const makeSut = () => new CompareFieldsValidator('any_field', 'any_field_to_compare')
+
+describe('CompareFieldsValidator', () => {
+  it('should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ any_field: 'any_field_value', any_field_to_compare: 'any_other_value' })
+
+    expect(error).toEqual(new InvalidParamError('any_field'))
+  })
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ any_field: 'any_field_value', any_field_to_compare: 'any_field_value' })
+
+    expect(error).toBeFalsy()
+  })
+})
