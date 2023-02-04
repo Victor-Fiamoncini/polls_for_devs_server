0c78b2c11c697cb887a02b8bf33ad601456e905e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-02-04 18:48:11 -0300

    chore: add new path aliases to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -7,13 +7,18 @@ export default {
   coverageReporters: ['lcov', 'json'],
   moduleFileExtensions: ['js', 'ts', 'json', 'node'],
   moduleNameMapper: {
-    'src/(.*)': '<rootDir>/src/$1'
+    '@/data/(.*)': '<rootDir>/src/data/$1',
+    '@/domain/(.*)': '<rootDir>/src/domain/$1',
+    '@/infra/(.*)': '<rootDir>/src/infra/$1',
+    '@/main/(.*)': '<rootDir>/src/main/$1',
+    '@/presentation/(.*)': '<rootDir>/src/presentation/$1',
+    '@/validation/(.*)': '<rootDir>/src/validation/$1',
   },
   preset: '@shelf/jest-mongodb',
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.(spec|test).ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    '.+\\.ts$': 'ts-jest',
+  },
 } as Config
