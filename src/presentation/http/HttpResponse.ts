c8e34756ad7e07e6fd2297db21d9111690fcf3ab
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-11-07 21:15:07 -0300

    feat: add noContent http-response handler

--- src/presentation/http/HttpResponse.ts
+++ src/presentation/http/HttpResponse.ts
@@ -32,3 +32,8 @@ export const created = (data: any): HttpResponse => ({
 })
 
 export const ok = (data: any): HttpResponse => ({ statusCode: 200, body: data })
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null,
+})
